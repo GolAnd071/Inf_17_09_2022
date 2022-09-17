485355ab3f918327246956aa3a48abaad4a008fc
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2022-09-17 19:42:21 +0300

    Changed Graphics Script

--- Plots/Plots.py
+++ Plots/Plots.py
@@ -3,26 +3,23 @@ import os
 
 
 def main():
-    x = list()
-    y1 = list()
-    y2 = list()
-    y3 = list()
+    g = [list() for _ in range(4)]
+
     while True:
         try:
             with open('ResultsTest1.txt') as f:
                 for i in range(10, 10001, 10):
                     temp = list(map(float, f.readline().split()))
-                    x.append(temp[0])
-                    y1.append(temp[1])
-                    y2.append(temp[2])
-                    y3.append(0.1)
+                    for i in range(3):
+                        g[i].append(temp[i])
+                    g[3].append(0.1)
             break
         except FileNotFoundError:
             pass
     os.remove('ResultsTest1.txt')
-    plt.plot(x, y1)
-    plt.plot(x, y2)
-    plt.plot(x, y3)
+    plt.plot(g[0], g[1], color='r', label='SegmentState')
+    plt.plot(g[0], g[2], color='g', label='SetState')
+    plt.plot(g[0], g[3], color='b', label='AsymptoticValue')
     plt.show()
 
 
